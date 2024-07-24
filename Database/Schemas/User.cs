f14f9a4c67806a353d46ce72931aaba1bc54a6b9
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-07-24 23:32:48 +0300

    Add WatsonORM for Database

---
+++ Database/Schemas/User.cs
@@ -0,0 +1,68 @@
+﻿using Sunrise.Types.Enums;
+using Watson.ORM.Core;
+
+namespace Sunrise.Database;
+
+[Table("user")]
+public class UserSchema
+{
+    [Column( true, DataTypes.Int, false)]
+    public int Id { get; set; }
+    
+    [Column(  DataTypes.Nvarchar, 64, false)]
+    public string Username { get; set; }
+    
+    [Column(  DataTypes.Nvarchar, 64, false)]
+    public string Passhash { get; set; }
+    
+    [Column( DataTypes.Nvarchar, 64, false)]
+    public string Token { get; set; }
+    
+    [Column( DataTypes.Int, false)]
+    public short Country { get; set; }
+    
+    [Column( DataTypes.Int, false)]
+    public UserPrivileges Privilege { get; set; }
+
+    [Column(DataTypes.Decimal, maxLength: 100, precision: 2, false)]
+    public float Accuracy { get; set; } = 0;
+    
+    [Column( DataTypes.Double, maxLength: 45, precision: 2, false)]
+    public long TotalScore { get; set; } = 0;
+    
+    [Column(DataTypes.Double,  maxLength: 45, precision: 2, false)]
+    public long RankedScore { get; set; } = 0;
+    
+    [Column( DataTypes.Int, false)]
+    public int PlayCount { get; set; } = 0;
+    
+    [Column( DataTypes.Int, false)]
+    public int PerformancePoints { get; set; } = 0;
+    
+    [Column( DataTypes.Int, false)]
+    public int PlayTime { get; set; } = 0;
+    
+    [Column(DataTypes.DateTime, false)]
+    public DateTime RegisterDate { get; set; } = DateTime.Now;
+    
+    public UserSchema()
+    {
+    }
+    
+    public UserSchema SetUserStats(string username, string passhash, string token, short country, UserPrivileges privilege)
+    {
+        Username = username;
+        Passhash = passhash;
+        Token = token;
+        Country = country;
+        Privilege = privilege;
+        RegisterDate = DateTime.Now;
+        return this;
+    }
+
+    public string UpdateToken()
+    {
+        Token = Guid.NewGuid().ToString();
+        return Token;
+    }
+}
--- Database/Types/Enums/FileType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sunrise.Database.Types.Enums;
-
-public enum FileType
-{
-    Avatar = 0,
-    Beatmap = 1,
-    Replay = 2,
-    Other = 99 // Not recommended to use this
-}
