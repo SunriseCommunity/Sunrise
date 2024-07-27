462f0f1611c3070f477a565318cb2a9d4bf24a86
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-07-27 10:33:42 +0300

    Remove token from UserScheme, rename to just User

--- Database/Schemas/User.cs
+++ Database/Schemas/User.cs
@@ -1,68 +1,58 @@
-﻿using Sunrise.Types.Enums;
+﻿using osu.Shared;
 using Watson.ORM.Core;
 
-namespace Sunrise.Database;
+namespace Sunrise.Database.Schemas;
 
 [Table("user")]
-public class UserSchema
+public class User
 {
-    [Column( true, DataTypes.Int, false)]
+    [Column(true, DataTypes.Int, false)]
     public int Id { get; set; }
-    
-    [Column(  DataTypes.Nvarchar, 64, false)]
+
+    [Column(DataTypes.Nvarchar, 64, false)]
     public string Username { get; set; }
-    
-    [Column(  DataTypes.Nvarchar, 64, false)]
+
+    [Column(DataTypes.Nvarchar, 64, false)]
     public string Passhash { get; set; }
-    
-    [Column( DataTypes.Nvarchar, 64, false)]
-    public string Token { get; set; }
-    
-    [Column( DataTypes.Int, false)]
+
+    [Column(DataTypes.Int, false)]
     public short Country { get; set; }
-    
-    [Column( DataTypes.Int, false)]
-    public UserPrivileges Privilege { get; set; }
+
+    [Column(DataTypes.Int, false)]
+    public PlayerRank Privilege { get; set; }
 
     [Column(DataTypes.Decimal, maxLength: 100, precision: 2, false)]
     public float Accuracy { get; set; } = 0;
-    
-    [Column( DataTypes.Double, maxLength: 45, precision: 2, false)]
+
+    [Column(DataTypes.Double, maxLength: 45, precision: 2, false)]
     public long TotalScore { get; set; } = 0;
-    
-    [Column(DataTypes.Double,  maxLength: 45, precision: 2, false)]
+
+    [Column(DataTypes.Double, maxLength: 45, precision: 2, false)]
     public long RankedScore { get; set; } = 0;
-    
-    [Column( DataTypes.Int, false)]
+
+    [Column(DataTypes.Int, false)]
     public int PlayCount { get; set; } = 0;
-    
-    [Column( DataTypes.Int, false)]
-    public int PerformancePoints { get; set; } = 0;
-    
-    [Column( DataTypes.Int, false)]
+
+    [Column(DataTypes.Int, false)]
+    public short PerformancePoints { get; set; } = 0;
+
+    [Column(DataTypes.Int, false)]
     public int PlayTime { get; set; } = 0;
-    
+
     [Column(DataTypes.DateTime, false)]
     public DateTime RegisterDate { get; set; } = DateTime.Now;
-    
-    public UserSchema()
+
+    public User()
     {
     }
-    
-    public UserSchema SetUserStats(string username, string passhash, string token, short country, UserPrivileges privilege)
+
+    public User SetUserStats(string username, string passhash, short country, PlayerRank privilege)
     {
         Username = username;
         Passhash = passhash;
-        Token = token;
         Country = country;
         Privilege = privilege;
         RegisterDate = DateTime.Now;
         return this;
-    }
-
-    public string UpdateToken()
-    {
-        Token = Guid.NewGuid().ToString();
-        return Token;
     }
 }
