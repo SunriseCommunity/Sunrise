8c6f4c545a0dbea613d8655d88ca0a39c4161bab
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-07-27 10:32:50 +0300

    Add moved files from WebClient to GameClient; Rename some controllers; Add menu-content endpoint

---
+++ GameClient/Controllers/AssetsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sunrise.GameClient.Services;
+
+namespace Sunrise.GameClient.Controllers;
+
+[ApiController]
+[Route("/assets")]
+public class AssetsController : ControllerBase
+{
+    private readonly FileService _fileService;
+
+    public AssetsController(FileService fileService)
+    {
+        _fileService = fileService;
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public async Task<IActionResult> GetAvatar(int id)
+    {
+        try
+        {
+            var result = await _fileService.GetAvatarBytes(id);
+            return new FileContentResult(result, "image/png");
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpGet]
+    [Route("menu-content.json")]
+    public IActionResult GetMenuContent()
+    {
+        const string eventImageUri = "https://osu.ppy.sh/api/events/EventBanner.jpg";
+
+        var json = """{ "images": [{ "image": "{img}", "url": "https://github.com/SunriseCommunity/Sunrise", "IsCurrent": true, "begins": null, "expires": "2099-06-01T12:00:00+00:00"}] }""";
+        json = json.Replace("{img}", eventImageUri);
+
+        return Ok(json);
+    }
+}
