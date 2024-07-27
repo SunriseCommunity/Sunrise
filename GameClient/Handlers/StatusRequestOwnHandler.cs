d55ec68c8369d187925bbf729c290b9f6ad568d4
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-07-27 10:49:09 +0300

    Update StatusRequestOwnHandler to use new session object

--- GameClient/Handlers/StatusRequestOwnHandler.cs
+++ GameClient/Handlers/StatusRequestOwnHandler.cs
@@ -1,13 +1,12 @@
 using HOPEless.Bancho;
-using Sunrise.Services;
-using Sunrise.Types.Interfaces;
+using Sunrise.GameClient.Objects;
+using Sunrise.GameClient.Types.Interfaces;
 
-namespace Sunrise.Handlers;
-
+namespace Sunrise.GameClient.Handlers;
 public class StatusRequestOwnHandler : IHandler
 {
-    public void Handle(BanchoPacket packet, BanchoService banchoSession, ServicesProvider services)
+    public void Handle(BanchoPacket packet, Session session, ServicesProvider services)
     {
-        banchoSession.SendUserStats();
+        session.SendUserData();
     }
 }
