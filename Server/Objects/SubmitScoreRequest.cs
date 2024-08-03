b7f6ca9b36052f8bba294d05b13c5b28a51abd34
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-08-03 14:54:09 +0300

    Implement requests used to simplify services query/form of requests

---
+++ Server/Objects/SubmitScoreRequest.cs
@@ -0,0 +1,21 @@
+namespace Sunrise.Server.Objects;
+
+public class SubmitScoreRequest(HttpRequest request)
+{
+    public string? ScoreEncoded { get; set; } = request.Form["score"];
+    public IFormFile? Replay { get; set; } = request.Form.Files["score"];
+    public string? OsuVersion { get; set; } = request.Form["osuver"];
+    public string? Iv { get; set; } = request.Form["iv"];
+    public string? PassHash { get; set; } = request.Form["pass"];
+    public string? ScoreTime { get; set; } = request.Form["st"];
+    public string? ScoreFailTime { get; set; } = request.Form["ft"];
+    public  string? BeatmapHash { get; set; } = request.Form["bmk"];
+    
+    public void ThrowIfHasEmptyFields()
+    {
+        if (string.IsNullOrEmpty(ScoreEncoded) || string.IsNullOrEmpty(OsuVersion) || string.IsNullOrEmpty(Iv) || string.IsNullOrEmpty(PassHash)  || string.IsNullOrEmpty(BeatmapHash) || Replay == null)
+        {
+            throw new Exception("Invalid request: Missing parameters");
+        }
+    }
+}
