97f70f89f5dc8d5a21904444fe34a93c323903d1
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-08-03 14:45:32 +0300

    Move Program.cs file; Update Program.cs file

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,11 +1,9 @@
 using Microsoft.Extensions.FileProviders;
-using Sunrise;
-using Sunrise.Database;
-using Sunrise.GameClient.Controllers;
-using Sunrise.GameClient.Repositories;
-using Sunrise.GameClient.Services;
-using Sunrise.WebClient.Controllers;
-using Sunrise.WebClient.Services;
+using Sunrise.Server.Controllers;
+using Sunrise.Server.Data;
+using Sunrise.Server.Repositories;
+using Sunrise.Server.Services;
+using Sunrise.Server.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,18 +14,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSingleton<RedisRepository>();
 builder.Services.AddSingleton<SessionRepository>();
-builder.Services.AddSingleton<Database>();
+builder.Services.AddSingleton<SunriseDb>();
 
 builder.Services.AddScoped<ServicesProvider>();
 builder.Services.AddScoped<ScoreService>();
 builder.Services.AddScoped<FileService>();
-builder.Services.AddScoped<ApiService>();
+builder.Services.AddScoped<BaseApiService>();
 
 builder.Services.AddScoped<BanchoController>();
 builder.Services.AddScoped<AssetsController>();
+builder.Services.AddScoped<BeatmapService>();
 builder.Services.AddScoped<WebController>();
-builder.Services.AddScoped<ApiController>();
+builder.Services.AddScoped<BaseApiController>();
 
 var app = builder.Build();
 
@@ -39,7 +39,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseStaticFiles(new StaticFileOptions
 {
-    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Database/Files/SeasonalBackgrounds")),
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Data/Files/SeasonalBackgrounds")),
     RequestPath = "/static"
 });
 
