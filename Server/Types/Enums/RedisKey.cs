357cf5dbf5f2f7c22ffa6b7feccfad58266cb753
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-08-11 06:05:02 +0300

    Minor changes

--- Server/Types/Enums/RedisKey.cs
+++ Server/Types/Enums/RedisKey.cs
@@ -1,7 +1,8 @@
 namespace Sunrise.Server.Types.Enums;
 
-public class RedisKey
+public static class RedisKey
 {
+    public const string OsuVersion = "osu:version:{0}";
 
     public const string User = "user:{0}";
     public const string UserStats = "user:{0}:stats:{1}";
@@ -10,6 +11,8 @@ public class RedisKey
     public const string BeatmapHash = "beatmap:hash:{0}";
     public const string Beatmap = "beatmap:{0}";
     public const string BeatmapSet = "beatmapset:{0}";
+    public const string BeatmapSetBySet = "beatmapset:byset:{0}"; // lol nice naming
+    public const string BeatmapSearch = "beatmapset:serach:{0}";
     public const string BeatmapFile = "beatmap:{0}:file";
 
     public const string Avatar = "avatar:{0}";
