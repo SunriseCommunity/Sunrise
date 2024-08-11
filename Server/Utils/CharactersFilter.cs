ec699dafc35f82c1b4523866a0d760a96e470338
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-08-11 06:01:31 +0300

    Add ChatChannel object, some other changes small

--- Server/Utils/CharactersFilter.cs
+++ Server/Utils/CharactersFilter.cs
@@ -6,7 +6,7 @@ public static class CharactersFilter
 {
     public static bool IsValidString(string str, bool allowRussian = false)
     {
-        var pattern = allowRussian ? @"^[a-zA-Z0-9а-яА-Я!@#$%^&*()_+]+$" : @"^[a-zA-Z0-9!@#$%^&*()_+]+$";
+        var pattern = allowRussian ? @"^[a-zA-Z0-9а-яА-Я!@#$%^&*()._+]+$" : @"^[a-zA-Z0-9!@#$%^&*()._+]+$";
 
         return Regex.IsMatch(str, pattern);
     }
