508ab7ecfca90488b066ad7586011ed3e3594500
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-08-12 01:12:01 +0300

    Add whois command, fix whoami having logic for global usage

--- Server/Objects/ChatCommands/WhoamiCommand.cs
+++ Server/Objects/ChatCommands/WhoamiCommand.cs
@@ -9,14 +9,7 @@ public class WhoamiCommand : IChatCommand
 {
     public Task Handle(Session session, ChatChannel? channel, string[]? args)
     {
-        if (channel != null)
-        {
-            channel.SendToChannel($"You are {session.User.Username}. Your ID is {session.User.Id}. Your privileges are {session.User.Privilege}.");
-        }
-        else
-        {
-            CommandRepository.SendMessage(session, $"You are {session.User.Username}. Your ID is {session.User.Id}. Your privileges are {session.User.Privilege}.");
-        }
+        CommandRepository.SendMessage(session, $"You are {session.User.Username}. Your ID is {session.User.Id}. Your privileges are {session.User.Privilege}.");
 
         return Task.CompletedTask;
     }
