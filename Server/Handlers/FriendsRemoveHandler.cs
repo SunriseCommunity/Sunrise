1cf7253cffa39916c3ea99fa1bce9b1dece0e8ab
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-08-11 07:19:56 +0300

    Add handlers for friend add/remove

---
+++ Server/Handlers/FriendsRemoveHandler.cs
@@ -0,0 +1,24 @@
+using HOPEless.Bancho;
+using HOPEless.Bancho.Objects;
+using Sunrise.Server.Data;
+using Sunrise.Server.Objects;
+using Sunrise.Server.Objects.CustomAttributes;
+using Sunrise.Server.Types.Interfaces;
+using Sunrise.Server.Utils;
+
+namespace Sunrise.Server.Handlers;
+
+[PacketHandler(PacketType.ClientFriendsRemove)]
+public class FriendsRemoveHandler : IHandler
+{
+    public async Task Handle(BanchoPacket packet, Session session)
+    {
+        var friendId = new BanchoInt(packet.Data);
+
+        await session.FetchUser();
+
+        session.User.RemoveFriend(friendId.Value);
+
+        await ServicesProviderHolder.ServiceProvider.GetRequiredService<SunriseDb>().UpdateUser(session.User);
+    }
+}
