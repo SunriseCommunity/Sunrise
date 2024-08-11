476bf038c8d11bbc6eb87f8ef8bcfd3c46e427ce
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-08-11 06:00:22 +0300

    Add email field to user; +For bot's id get Bot.png

--- Server/Objects/Models/User.cs
+++ Server/Objects/Models/User.cs
@@ -8,19 +8,22 @@ public class User
 {
     [Column(true, DataTypes.Int, false)]
     public int Id { get; set; }
-    
+
     [Column(DataTypes.Nvarchar, 64, false)]
     public string Username { get; set; }
-    
+
+    [Column(DataTypes.Nvarchar, 64, false)]
+    public string Email { get; set; }
+
     [Column(DataTypes.Nvarchar, 64, false)]
     public string Passhash { get; set; }
-    
+
     [Column(DataTypes.Int, false)]
     public short Country { get; set; }
-    
+
     [Column(DataTypes.Int, false)]
     public PlayerRank Privilege { get; set; }
-    
+
     [Column(DataTypes.DateTime, false)]
     public DateTime RegisterDate { get; set; } = DateTime.UtcNow;
 }
