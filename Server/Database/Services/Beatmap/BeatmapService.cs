ab56aaf3b27ab8b4b7df98d27837ccc0e94a0a0f
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-11-25 12:41:57 +0200

    feat: Dont save duplicated beatmapsets for different pointers, make pointers point to set_id; chore: Move redis logic to BeatmapManager

--- Server/Database/Services/Beatmap/BeatmapService.cs
+++ Server/Database/Services/Beatmap/BeatmapService.cs
@@ -1,5 +1,8 @@
+using Sunrise.Server.Application;
 using Sunrise.Server.Database.Services.Beatmap.Services;
+using Sunrise.Server.Objects.Serializable;
 using Sunrise.Server.Repositories;
+using Sunrise.Server.Types;
 using Watson.ORM.Sqlite;
 
 namespace Sunrise.Server.Database.Services.Beatmap;
@@ -24,5 +27,67 @@ public class BeatmapService
 
     public BeatmapFileService Files { get; }
 
-    // TODO: REDIS SAVING LOGIC HERE
+    public async Task<BeatmapSet?> GetCachedBeatmapSet(int? beatmapSetId = null, string? beatmapHash = null, int? beatmapId = null)
+    {
+        if (beatmapSetId == null && beatmapHash == null && beatmapId == null) return null;
+
+        if (beatmapId != null)
+            return await GetCachedBeatmapSetByBeatmapId(beatmapId.Value);
+        if (beatmapHash != null)
+            return await GetCachedBeatmapSetByBeatmapHash(beatmapHash);
+        if (beatmapSetId != null)
+            return await GetCachedBeatmapSetBySetId(beatmapSetId.Value);
+
+        return null;
+    }
+
+    public async Task SetCachedBeatmapSet(BeatmapSet beatmapSet)
+    {
+        var expiry = TimeSpan.FromHours(1); // TODO: Copy from observatory caching logic, but lower
+
+        await _redis.Set(RedisKey.BeatmapSetBySetId(beatmapSet.Id), beatmapSet, expiry);
+
+        foreach (var b in beatmapSet.Beatmaps)
+        {
+            await _redis.Set([RedisKey.BeatmapSetIdByHash(b.Checksum), RedisKey.BeatmapSetIdByBeatmapId(b.Id)],
+                beatmapSet.Id,
+                expiry);
+        }
+    }
+
+    private async Task<BeatmapSet?> GetCachedBeatmapSetByBeatmapId(int beatmapId)
+    {
+        var beatmapSetId = await _redis.Get<int?>(RedisKey.BeatmapSetIdByBeatmapId(beatmapId));
+
+        if (!beatmapSetId.HasValue) return null;
+
+        return await GetCachedBeatmapSetBySetId(beatmapSetId.Value);
+    }
+
+    private async Task<BeatmapSet?> GetCachedBeatmapSetByBeatmapHash(string beatmapHash)
+    {
+        var beatmapSetId = await _redis.Get<int?>(RedisKey.BeatmapSetIdByHash(beatmapHash));
+
+        if (!beatmapSetId.HasValue) return null;
+
+        return await GetCachedBeatmapSetBySetId(beatmapSetId.Value);
+    }
+
+    private async Task<BeatmapSet?> GetCachedBeatmapSetBySetId(int beatmapSetId)
+    {
+        var beatmapSet = await _redis.Get<BeatmapSet?>(RedisKey.BeatmapSetBySetId(beatmapSetId));
+
+        if (beatmapSet == null) return null;
+
+        // TODO: Have it as extension method
+        if (Configuration.IgnoreBeatmapRanking)
+        {
+            foreach (var b in beatmapSet.Beatmaps)
+            {
+                b.StatusString = "ranked";
+            }
+        }
+
+        return beatmapSet;
+    }
 }
