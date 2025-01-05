5abc486dfd7238e41c4c8feb52b20d3b79234e98
ItsRichardScull <totallynotrichardscull@gmail.com>
2025-01-05 14:35:23 +0200

    feat: Add BackupDatabaseCommand;

---
+++ Server/Chat/Commands/Development/BackupDatabaseCommand.cs
@@ -0,0 +1,37 @@
+using Sunrise.Server.Application;
+using Sunrise.Server.Attributes;
+using Sunrise.Server.Objects;
+using Sunrise.Server.Repositories.Attributes;
+using Sunrise.Server.Types.Enums;
+using Sunrise.Server.Types.Interfaces;
+
+namespace Sunrise.Server.Chat.Commands.Development;
+
+[ChatCommand("backupdatabase", requiredPrivileges: UserPrivileges.Developer)]
+public class BackupDatabaseCommand : IChatCommand
+{
+    public Task Handle(Session session, ChatChannel? channel, string[]? args)
+    {
+        if (Configuration.OnMaintenance)
+        {
+            CommandRepository.SendMessage(session, "Server is in maintenance mode. Starting database backup is not possible.");
+            return Task.CompletedTask;
+        }
+
+        CommandRepository.SendMessage(session, "Database backup has been started. Server is in maintenance mode.");
+
+        Configuration.OnMaintenance = true;
+
+        _ = StartDatabaseBackup(session);
+
+        return Task.CompletedTask;
+    }
+
+    private Task StartDatabaseBackup(Session session)
+    {
+        BackgroundTasks.BackupDatabase();
+        CommandRepository.SendMessage(session, "Database backup has been completed. Server is no longer in maintenance mode.");
+        Configuration.OnMaintenance = false;
+        return Task.CompletedTask;
+    }
+}
