14cc71e1a0550bb72c00d322edeb94b9ab5bcde2
ItsRichardScull <totallynotrichardscull@gmail.com>
2025-02-06 06:20:28 +0200

    chore: Clear up Data_Tests folder chore: Rename Data_Tests folder to DataTest

--- Data_Tests/Migrations/000007_login_events.sql
+++
@@ -1,9 +0,0 @@
--- 1. Add new table 
-CREATE TABLE IF NOT EXISTS `login_event`
-(
-    `Id`        INTEGER PRIMARY KEY AUTOINCREMENT  NOT NULL,
-    `UserId`    INTEGER                            NOT NULL,
-    `Ip`        VARCHAR(64) COLLATE NOCASE         NOT NULL,
-    `LoginData` VARCHAR(2147483647) COLLATE NOCASE NOT NULL,
-    `Time`      TEXT                               NOT NULL
-)
