fb9ede5f265a66f154747febff5e71bcae8aa9bb
ItsRichardScull <totallynotrichardscull@gmail.com>
2025-02-06 09:30:41 +0200

    feat: Add ApiBaseLimitsTests;

--- Sunrise.Server.Tests/API/BaseController/ApiBaseLimitsTests.cs
+++ Sunrise.Server.Tests/API/BaseController/ApiBaseLimitsTests.cs
@@ -1,70 +1,75 @@
 using System.Text.Json;
 using Sunrise.Server.API.Serializable.Response;
 using Sunrise.Server.Application;
-using Sunrise.Server.Database;
-using Sunrise.Server.Repositories;
 using Sunrise.Server.Tests.Core.Abstracts;
 using Sunrise.Server.Tests.Core.Utils;
 
 namespace Sunrise.Server.Tests.API.BaseController;
 
-public class ApiBaseStatusTests : ApiTest
+public class ApiBaseLimitsTests : ApiTest
 {
     [Fact]
-    public async Task TestStatusReturnsValidInfo()
+    public async Task TestLimitsReturnsValidInfo()
     {
         // Arrange
         await using var app = new SunriseServerFactory();
         var client = app.CreateClient().UseClient("api");
+        var ip = MockUtil.GetRandomIp();
         
         // Act
-        var response = await client.GetAsync("/status");
+        var response = await client.UseUserIp(ip).GetAsync("/limits");
         
         // Assert
         response.EnsureSuccessStatusCode();
         
         var responseString = await response.Content.ReadAsStringAsync();
-        var status = JsonSerializer.Deserialize<StatusResponse>(responseString);
-        
+        var status = JsonSerializer.Deserialize<LimitsResponse>(responseString);
         Assert.NotNull(status);
         
-        var database = ServicesProviderHolder.GetRequiredService<DatabaseManager>();
-        var sessions = ServicesProviderHolder.GetRequiredService<SessionRepository>();
-        
-        var usersOnline = sessions.GetSessions().Count;
-        var totalUsers = await database.UserService.GetTotalUsers();
-        
-        Assert.Equal(usersOnline, status.UsersOnline);
-        Assert.Equal(totalUsers, status.TotalUsers);
+        var currentRateLimit = status.RateLimitsObj.TotalLimit - 1;
+        Assert.True(currentRateLimit == status.RateLimitsObj.RemainingCalls);
     }
     
     [Fact]
-    public async Task TestStatusDetailedReturnsValidInfo()
+    public async Task TestLimitsReturnsValidInfoAfterTwoRequests()
     {
         // Arrange
         await using var app = new SunriseServerFactory();
         var client = app.CreateClient().UseClient("api");
+        var ip = MockUtil.GetRandomIp();
         
         // Act
-        var response = await client.GetAsync("/status?detailed=true");
+        await client.UseUserIp(ip).GetAsync("/limits");
+        var response = await client.UseUserIp(ip).GetAsync("/limits");
         
         // Assert
         response.EnsureSuccessStatusCode();
         
         var responseString = await response.Content.ReadAsStringAsync();
-        var status = JsonSerializer.Deserialize<StatusResponse>(responseString);
-        
+        var status = JsonSerializer.Deserialize<LimitsResponse>(responseString);
         Assert.NotNull(status);
         
-        var database = ServicesProviderHolder.GetRequiredService<DatabaseManager>();
-        var sessions = ServicesProviderHolder.GetRequiredService<SessionRepository>();
+        var currentRateLimit = status.RateLimitsObj.TotalLimit - 2;
+        Assert.True(currentRateLimit == status.RateLimitsObj.RemainingCalls);
+    }
+    
+    [Fact]
+    public async Task TestLimitsReturnsTooManyRequestsAfterGettingOverLimit()
+    {
+        // Arrange
+        await using var app = new SunriseServerFactory();
+        var client = app.CreateClient().UseClient("api");
+        var ip = MockUtil.GetRandomIp();
         
-        var usersOnline = sessions.GetSessions().Count;
-        var totalUsers = await database.UserService.GetTotalUsers();
-        var totalScores = await database.ScoreService.GetTotalScores();
+        // Act
+        for (var i = 0; i < Configuration.GeneralCallsPerWindow; i++)
+        {
+            await client.UseUserIp(ip).GetAsync("/limits");
+        }
         
-        Assert.Equal(usersOnline, status.UsersOnline);
-        Assert.Equal(totalUsers, status.TotalUsers);
-        Assert.Equal(totalScores, status.TotalScores);
+        var response = await client.UseUserIp(ip).GetAsync("/limits");
+        
+        // Assert
+        Assert.Equal(System.Net.HttpStatusCode.TooManyRequests, response.StatusCode);
     }
 }
--- Sunrise.Server/API/Serializable/Response/LimitsResponse.cs
+++ Sunrise.Server/API/Serializable/Response/LimitsResponse.cs
@@ -3,35 +3,52 @@ using Sunrise.Server.Application;
 
 namespace Sunrise.Server.API.Serializable.Response;
 
-public class LimitsResponse(long? remainingCalls, int? remainingBeatmapRequests)
+public class RateLimits
+{
+    [JsonPropertyName("total_limit")] public int TotalLimit { get; set; }
+
+    [JsonPropertyName("remaining_calls")] public long RemainingCalls { get; set; }
+}
+
+public class LimitsResponse
 {
     private readonly int _beatmapRequestLimit = Configuration.ApiCallsPerWindow;
-    private readonly int _remainingBeatmapRequests = remainingBeatmapRequests ?? Configuration.ApiCallsPerWindow;
-    private readonly long _remainingCalls = remainingCalls ?? Configuration.GeneralCallsPerWindow;
+
     private readonly int _totalLimit = Configuration.GeneralCallsPerWindow;
 
+    public LimitsResponse(long? remainingCallsValue, int? remainingBeatmapRequestsValue)
+    {
+        var remainingCalls = remainingCallsValue ?? Configuration.GeneralCallsPerWindow;
+        var remainingBeatmapRequests = remainingBeatmapRequestsValue ?? Configuration.ApiCallsPerWindow;
+       
+        RateLimitsObj = new RateLimits
+        {
+            TotalLimit = _totalLimit,
+            RemainingCalls = remainingCalls
+        };
+       
+        RateLimitsBeatmap  = new RateLimits
+        {
+            TotalLimit = _beatmapRequestLimit,
+            RemainingCalls = remainingBeatmapRequests
+        };
+    }
+    
+    [JsonConstructor]
+    public LimitsResponse(RateLimits rateLimitsObj, RateLimits rateLimitsBeatmap, string message)
+    {
+        RateLimitsObj = rateLimitsObj;
+        RateLimitsBeatmap = rateLimitsBeatmap;
+        Message = message;
+    }
+
     [JsonPropertyName("message")]
-    public string Message { get; } =
+    public string Message { get; set; } =
         "Total limits are any requests to the server. Beatmap request limits are requests which involve beatmap retrieval. Keep in mind that beatmap requests only counts towards new beatmaps to our database.";
 
     [JsonPropertyName("rate_limits")]
-    public RateLimits RateLimitsObj => new()
-    {
-        TotalLimit = _totalLimit,
-        RemainingCalls = _remainingCalls
-    };
+    public RateLimits RateLimitsObj { get; set; }
 
     [JsonPropertyName("beatmap_rate_limits")]
-    public RateLimits RateLimitsBeatmap => new()
-    {
-        TotalLimit = _beatmapRequestLimit,
-        RemainingCalls = _remainingBeatmapRequests
-    };
-
-    public class RateLimits
-    {
-        [JsonPropertyName("total_limit")] public int TotalLimit { get; set; }
-
-        [JsonPropertyName("remaining_calls")] public long RemainingCalls { get; set; }
-    }
+    public RateLimits RateLimitsBeatmap { get; set; } 
 }
