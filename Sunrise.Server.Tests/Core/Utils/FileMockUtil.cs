fc54e3441b4b08de7fe42481ef670c4b046c22ae
ItsRichardScull <totallynotrichardscull@gmail.com>
2025-02-06 08:46:19 +0200

    feat: Add ImageToolsTests; feat: Update FileMockUtils to recursively search for all files; chore: Update and sort test resources by folders;

--- Sunrise.Server.Tests/Core/Utils/FileMockUtil.cs
+++ Sunrise.Server.Tests/Core/Utils/FileMockUtil.cs
@@ -1,21 +1,43 @@
 ﻿
 namespace Sunrise.Server.Tests.Core.Utils;
 
+public class FileSizeFilter
+{
+    public long? MaxSize { get; set; }
+    public long? MinSize { get; set; }
+}
+
 public static class FileMockUtil
 {
     private static readonly string ResoursesPath = Path.Combine(Directory.GetCurrentDirectory(), "Sunrise.Server.Tests/Core/Resources");
     
     public static string GetRandomFilePath()
     {
-        var files = Directory.GetFiles(ResoursesPath);
+        var files = GetAllFilesRecursively(ResoursesPath);
         var random = new Random();
         return files[random.Next(files.Length)];
     }
     
-    public static string GetRandomImageFilePath(string extension)
+    public static string GetRandomFilePath(string extension, FileSizeFilter? fileSize = null)
     {
-        var files = Directory.GetFiles(Path.Combine( ResoursesPath, "Images"), $"*.{extension}");
+        var files = GetAllFilesRecursively(ResoursesPath, $"*.{extension}", fileSize);
         var random = new Random();
         return files[random.Next(files.Length)];
+    }
+
+    public static string[] GetAllFilesRecursively(string path, string searchPattern = "*", FileSizeFilter? fileSize = null)
+    {
+        var files = Directory.GetFiles(path, searchPattern, SearchOption.AllDirectories);
+        
+        if (fileSize == null)
+            return files;
+
+        if (fileSize.MaxSize != null)
+            files = files.Where(f => new FileInfo(f).Length <= fileSize.MaxSize).ToArray();
+            
+        if (fileSize.MinSize != null)
+            files = files.Where(f => new FileInfo(f).Length >= fileSize.MinSize).ToArray();
+
+        return files;
     }
 }
--- Sunrise.Server.Tests/Utils/ImageToolsTests.cs
+++ Sunrise.Server.Tests/Utils/ImageToolsTests.cs
@@ -8,11 +8,13 @@ namespace Sunrise.Server.Tests.Utils;
 
 public class ImageToolsTests : FilesystemTest
 {
+    private const int Megabyte = 1024 * 1024;
+    
     [Fact]
     public void TestResizeImage()
     {
         // Arrange
-        var imagePath = FileMockUtil.GetRandomImageFilePath("jpg");
+        var imagePath = FileMockUtil.GetRandomFilePath("jpg");
         
         var imageBytes = File.ReadAllBytes(imagePath);
         var size = new Size(100, 100);
@@ -23,5 +25,144 @@ public class ImageToolsTests : FilesystemTest
         // Assert
         using var image = Image.Load(resizedImage);
         Assert.Equal(size, image.Size);
+    }
+    
+    [Fact]
+    public void TestResizeImageWithInvalidSize()
+    {
+        // Arrange
+        var imagePath = FileMockUtil.GetRandomFilePath("jpg");
+        
+        var imageBytes = File.ReadAllBytes(imagePath);
+        var size = new Size(0, -100);
+        
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => ImageTools.ResizeImage(imageBytes, size.Height, size.Width));
+    }
+    
+    [Fact]
+    public void TestResizeImageWithInvalidImage()
+    {
+        // Arrange
+        var imageBytes = Array.Empty<byte>();
+        var size = new Size(100, 100);
+        
+        // Act & Assert
+        Assert.Throws<UnknownImageFormatException>(() => ImageTools.ResizeImage(imageBytes, size.Height, size.Width));
+    }
+
+    [Fact]
+    public void TestResizeImageWithNotImage()
+    {
+        // Arrange
+        var textFilePath = FileMockUtil.GetRandomFilePath("txt");
+        
+        var imageBytes = File.ReadAllBytes(textFilePath);
+        var size = new Size(100, 100);
+        
+        // Act & Assert
+        Assert.Throws<UnknownImageFormatException>(() => ImageTools.ResizeImage(imageBytes, size.Height, size.Width));
+    }
+    
+    [Fact]
+    public void TestIsValidImageCheckValidImage()
+    {
+        // Arrange
+        var imagePath = FileMockUtil.GetRandomFilePath("jpg");
+        var imageBytes = File.ReadAllBytes(imagePath);
+        
+        // Act
+        var result = ImageTools.IsValidImage(new MemoryStream(imageBytes));
+        
+        // Assert
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public void TestIsValidImageCheckInvalidImage()
+    {
+        // Arrange
+        var textFilePath = FileMockUtil.GetRandomFilePath("txt");
+        var imageBytes = File.ReadAllBytes(textFilePath);
+        
+        // Act
+        var result = ImageTools.IsValidImage(new MemoryStream(imageBytes));
+        
+        // Assert
+        Assert.False(result);
+    }
+    
+    [Theory]
+    [InlineData("jpg")]
+    [InlineData("png")]
+    [InlineData("gif")]
+    [InlineData("ico")]
+    public void TestGetImageType(string extension)
+    {
+        // Arrange
+        var imagePath = FileMockUtil.GetRandomFilePath(extension);
+        var imageBytes = File.ReadAllBytes(imagePath);
+        
+        // Act
+        var result = ImageTools.GetImageType(imageBytes);
+        
+        // Assert
+        Assert.Equal(extension, result);
+    }
+
+    [Fact]
+    public void IsHasValidImageAttributesWithValidImage()
+    {
+        // Arrange
+        var imagePath = FileMockUtil.GetRandomFilePath("jpg", new FileSizeFilter { MaxSize = Megabyte * 5 });
+        var imageBytes = File.ReadAllBytes(imagePath);
+        
+        // Act
+        var result = ImageTools.IsHasValidImageAttributes(new MemoryStream(imageBytes));
+        
+        // Assert
+        Assert.True(result.Item1);
+    }
+    
+    [Fact]
+    public void IsHasValidImageAttributesWithLargeImage()
+    {
+        // Arrange
+        var imagePath = FileMockUtil.GetRandomFilePath("png", new FileSizeFilter { MinSize = Megabyte * 5 });
+        var imageBytes = File.ReadAllBytes(imagePath);
+        
+        // Act
+        var result = ImageTools.IsHasValidImageAttributes(new MemoryStream(imageBytes));
+        
+        // Assert
+        Assert.False(result.Item1);
+    }
+    
+    [Fact]
+    public void IsHasValidImageAttributesWithInvalidImage()
+    {
+        // Arrange
+        var textFilePath = FileMockUtil.GetRandomFilePath("txt");
+        var imageBytes = File.ReadAllBytes(textFilePath);
+        
+        // Act
+        var result = ImageTools.IsHasValidImageAttributes(new MemoryStream(imageBytes));
+        
+        // Assert
+        Assert.False(result.Item1);
+    }
+
+    [Fact]
+    public void TestGetImageTypeWithInvalidImage()
+    {
+        // Arrange
+        var textFilePath = FileMockUtil.GetRandomFilePath("txt");
+        var imageBytes = File.ReadAllBytes(textFilePath);
+        
+        // Act
+        var result = ImageTools.GetImageType(imageBytes);
+        
+        // Assert
+        Assert.Null(result);
     }
 }
