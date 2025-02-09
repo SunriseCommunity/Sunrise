a025cf54654f2f16d085263a01256fd639990508
ItsRichardScull <totallynotrichardscull@gmail.com>
2025-02-09 09:06:41 +0200

    feat: Add ReplayService for Tests;

---
+++ Sunrise.Server.Tests/Core/Services/ReplayService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Sunrise.Server.Objects;
+using Sunrise.Server.Tests.Core.Services.Mock;
+
+namespace Sunrise.Server.Tests.Core.Services;
+
+public class ReplayService
+{
+    private readonly MockService _mocker = new();
+    
+    public IFormFile GenerateReplayFormFile()
+    {
+        return GenerateReplayFormFile(new byte[1024], $"{_mocker.GetRandomString(6)}.osr");
+    }
+    
+    public IFormFile GenerateReplayFormFile(ReplayFile replay)
+    {
+        var buffer = replay.GetReplayData();
+        
+        return GenerateReplayFormFile(buffer, "replay.osr");
+    }
+
+    private IFormFile GenerateReplayFormFile(byte[] buffer, string fileName)
+    {
+        IFormFile formFile = new FormFile(new MemoryStream(buffer), 0, buffer.Length, "data", fileName);
+        return formFile;
+    }
+}
