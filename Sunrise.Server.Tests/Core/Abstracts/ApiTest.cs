8c22c76d528bb76d09d1e22b1a4b2cde7000b20a
ItsRichardScull <totallynotrichardscull@gmail.com>
2025-02-10 09:46:13 +0200

    feat: Add useRedis for ApiTest

--- Sunrise.Server.Tests/Core/Abstracts/ApiTest.cs
+++ Sunrise.Server.Tests/Core/Abstracts/ApiTest.cs
@@ -4,7 +4,7 @@ using AuthService = Sunrise.Server.API.Services.AuthService;
 
 namespace Sunrise.Server.Tests.Core.Abstracts;
 
-public abstract class ApiTest : DatabaseTest
+public abstract class ApiTest(bool useRedis = false) : DatabaseTest(useRedis)
 {
     protected async Task<TokenResponse> GetUserAuthTokens(User? user = null)
     {
