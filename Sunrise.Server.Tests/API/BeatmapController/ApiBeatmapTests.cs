ed89a150017521c18798915a4a72b6755cf0eb42
ItsRichardScull <totallynotrichardscull@gmail.com>
2025-02-10 09:45:00 +0200

    test: Add ApiBeatmapTest.cs

---
+++ Sunrise.Server.Tests/API/BeatmapController/ApiBeatmapTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Net;
+using Sunrise.Server.Tests.Core.Abstracts;
+using Sunrise.Server.Tests.Core.Services.Mock;
+using Sunrise.Server.Tests.Core.Utils;
+
+namespace Sunrise.Server.Tests.API.BeatmapController;
+
+public class ApiBeatmapRedisTests() : ApiTest(true)
+{
+    private readonly MockService _mocker = new();
+
+    [Fact]
+    public async Task TestGetBeatmap()
+    {
+        // Arrange
+        await using var app = new SunriseServerFactory();
+        var client = app.CreateClient().UseClient("api");
+
+        var beatmapSet = _mocker.Beatmap.GetRandomBeatmapSet();
+        var beatmap = beatmapSet.Beatmaps.First() ?? throw new Exception("Beatmap is null");
+        beatmap.Id = 1;
+
+        await _mocker.Beatmap.MockBeatmapSet(beatmapSet);
+
+        // Act
+        var response = await client.GetAsync("beatmap/1");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
+
+public class ApiBeatmapTests : ApiTest
+{
+    [Theory]
+    [InlineData("-1")]
+    [InlineData("test")]
+    public async Task TestGetBeatmapInvalidBeatmapId(string beatmapId)
+    {
+        // Arrange
+        await using var app = new SunriseServerFactory();
+        var client = app.CreateClient().UseClient("api");
+
+        // Act
+        var response = await client.GetAsync($"beatmap/{beatmapId}");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task TestGetBeatmapNotFound()
+    {
+        // Arrange
+        await using var app = new SunriseServerFactory();
+        var client = app.CreateClient().UseClient("api");
+
+        // Act
+        var response = await client.GetAsync("beatmap/1");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+}
