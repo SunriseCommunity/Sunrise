65861f11dfe628100e089b4d3258c1366ca0b96f
ItsRichardScull <totallynotrichardscull@gmail.com>
2025-02-12 11:25:10 +0200

    chore: Clear SunriseServerFactory.cs

--- Sunrise.Server.Tests/SunriseServerFactory.cs
+++ Sunrise.Server.Tests/SunriseServerFactory.cs
@@ -1,10 +1,7 @@
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Sunrise.Server.Tests.Core;
 
 namespace Sunrise.Server.Tests;
 
-internal class SunriseServerFactory : WebApplicationFactory<Program>, IClassFixture<EnvironmentFixture>
+internal class SunriseServerFactory : WebApplicationFactory<Program>
 {
-
 }
