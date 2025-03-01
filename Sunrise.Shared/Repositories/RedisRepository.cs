6051e6da56532963360c540fdfa964138f2c54e5
ItsRichardScull <totallynotrichardscull@gmail.com>
2025-03-01 23:27:43 +0200

    feat: Add SortedSetRangeByRankAsync method to RedisRepository; feat: Move sorted sets to other redis database; feat: Add flushOnlyGeneralDatabase option for redis flush method;

--- Sunrise.Shared/Repositories/RedisRepository.cs
+++ Sunrise.Shared/Repositories/RedisRepository.cs
@@ -1,20 +1,23 @@
 using System.Text.Json;
+using CSharpFunctionalExtensions;
 using StackExchange.Redis;
 using Sunrise.Shared.Application;
+using Sunrise.Shared.Utils;
 
 namespace Sunrise.Shared.Repositories;
 
 public class RedisRepository
 {
     private static readonly ConnectionMultiplexer RedisConnection =
-        ConnectionMultiplexer.Connect(Configuration.RedisConnection);
+        ConnectionMultiplexer.Connect($"{Configuration.RedisConnection},allowAdmin=true");
 
     private static readonly JsonSerializerOptions JsonSerializerOptions = new()
     {
         IncludeFields = true
     };
 
-    private readonly IDatabase _redis = RedisConnection.GetDatabase();
+    private readonly IDatabase _generalDatabase = RedisConnection.GetDatabase(0);
+    private readonly IDatabase _sortedSetsDatabase = RedisConnection.GetDatabase(1);
 
     private static bool UseCache => Configuration.UseCache;
 
@@ -22,7 +25,7 @@ public class RedisRepository
     {
         if (!UseCache) return default;
 
-        var value = await _redis.StringGetAsync(key);
+        var value = await _generalDatabase.StringGetAsync(key);
         return value.HasValue ? JsonSerializer.Deserialize<T>(value!, JsonSerializerOptions) : default;
     }
 
@@ -30,7 +33,7 @@ public class RedisRepository
     {
         if (!UseCache) return default;
 
-        var values = await _redis.StringGetAsync(keys.Select(x => (RedisKey)x).ToArray());
+        var values = await _generalDatabase.StringGetAsync(keys.Select(x => (RedisKey)x).ToArray());
 
         foreach (var value in values)
         {
@@ -45,56 +48,77 @@ public class RedisRepository
     {
         if (!UseCache) return;
 
-        await _redis.StringSetAsync(new RedisKey(key),
+        await _generalDatabase.StringSetAsync(new RedisKey(key),
             JsonSerializer.Serialize(value, JsonSerializerOptions),
-            cacheTime ?? TimeSpan.FromSeconds(Configuration.RedisCacheLifeTime));
+            cacheTime ?? TimeSpan.FromSeconds(Configuration.RedisCacheLifeTime),
+            flags: CommandFlags.FireAndForget);
     }
 
     public async Task Set<T>(string[] keys, T value, TimeSpan? cacheTime = null)
     {
         if (!UseCache) return;
 
+        List<Task> setTasks = [];
+
         foreach (var t in keys)
         {
-            await Set(t, value, cacheTime);
+            setTasks.Add(Set(t, value, cacheTime));
         }
+
+        await Task.WhenAll(setTasks);
     }
 
     public async Task Remove(string key)
     {
         if (!UseCache) return;
 
-        await _redis.KeyDeleteAsync(key);
+        await _generalDatabase.KeyDeleteAsync(key);
     }
 
     public async Task Remove(string[] keys)
     {
         if (!UseCache) return;
 
-        await _redis.KeyDeleteAsync(keys.Select(x => (RedisKey)x).ToArray());
+        await _generalDatabase.KeyDeleteAsync(keys.Select(x => (RedisKey)x).ToArray());
     }
 
-    public async Task<bool> SortedSetAdd(string key, int value, double score)
+    public async Task SortedSetAdd(string key, int value, double score)
     {
         var timestamp = long.MaxValue - DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        var redisValue = $"{timestamp}:{value}";
 
-        // Clear old values with the same key (because we have timestamp in key we can't just overwrite it)
-        await SortedSetRemove(key, value);
+        await foreach (var entry in _sortedSetsDatabase.SortedSetScanAsync(key, $"*:{value}"))
+        {
+            await _sortedSetsDatabase.SortedSetRemoveAsync(key, entry.Element, CommandFlags.FireAndForget);
+        }
 
-        return await _redis.SortedSetAddAsync(key, $"{timestamp}:{value}", score);
+        await _sortedSetsDatabase.SortedSetAddAsync(key, redisValue, score, CommandFlags.FireAndForget);
     }
+
+
+    public async Task<Result<List<long>>> SortedSetRangeByRankAsync(string key, long start = 0, long stop = long.MaxValue)
+    {
+        return await ResultUtil.TryExecuteAsync(async () =>
+        {
+            var users = await _sortedSetsDatabase.SortedSetRangeByRankAsync(
+                key,
+                start,
+                stop,
+                Order.Descending
+            );
+
+            var userIds = users.Select(value => value.ToString().Split(":").Last());
+
+            return userIds.Select(long.Parse).ToList();
+        });
+    }
+
 
     public async Task<long?> SortedSetRank(string key, int value)
     {
-        var entries = await _redis.SortedSetRangeByRankAsync(key);
-
-        foreach (var entry in entries)
+        await foreach (var entry in _sortedSetsDatabase.SortedSetScanAsync(key, $"*:{value}"))
         {
-            if (entry.ToString().EndsWith(":" + value))
-            {
-                var rank = await _redis.SortedSetRankAsync(key, entry, Order.Descending);
-                return rank;
-            }
+            return await _sortedSetsDatabase.SortedSetRankAsync(key, entry.Element, Order.Descending);
         }
 
         return null;
@@ -102,19 +126,21 @@ public class RedisRepository
 
     public async Task<bool> SortedSetRemove(string key, int value)
     {
-        var entries = await _redis.SortedSetRangeByRankAsync(key);
-
-        foreach (var entry in entries)
+        await foreach (var entry in _sortedSetsDatabase.SortedSetScanAsync(key, $"*:{value}"))
         {
-            if (entry.ToString().EndsWith(":" + value))
-                return await _redis.SortedSetRemoveAsync(key, entry);
+            return await _sortedSetsDatabase.SortedSetRemoveAsync(key, entry.Element);
         }
 
         return false;
     }
 
-    public void FlushAllCache()
+    public async Task Flush(bool flushOnlyGeneralDatabase = true)
     {
-        _redis.Execute("FLUSHALL");
+        var server = RedisConnection.GetServer(RedisConnection.GetEndPoints().FirstOrDefault());
+
+        if (flushOnlyGeneralDatabase)
+            await server.FlushDatabaseAsync(0);
+
+        await server.FlushAllDatabasesAsync();
     }
 }
