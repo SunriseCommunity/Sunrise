4e5e81b4286d55c2d80158fe865c4391a631c0ae
ItsRichardScull <totallynotrichardscull@gmail.com>
2025-03-01 23:33:43 +0200

    feat: Add new old type migration for wrongly unlifted restrictions;

---
+++ Data/Migrations/000021_revert_unlifted_restrictions.sql
@@ -0,0 +1,9 @@
+﻿-- Restricts all accidentally unrestricted users which were disabled
+
+UPDATE user
+SET AccountStatus = 1
+WHERE Id IN (
+    SELECT UserId FROM restriction 
+) AND AccountStatus = 2;
+
+SELECT 'Restrictions reverted'  AS TableName, changes() AS RevertedRestrictionsRows;
