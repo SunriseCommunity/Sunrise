a6f94fd7ff011107f5aeddac53582d85a9fc5fb0
ItsRichardScull <totallynotrichardscull@gmail.com>
2025-03-01 13:30:12 +0200

    feat: Add old type of migrations, to support migration from WatsonORM to EF

---
+++ Data/Migrations/000019_migrate_to_entity_framework.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+      "MigrationId"	TEXT NOT NULL,
+      "ProductVersion"	TEXT NOT NULL,
+      CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY("MigrationId")
+);
+
+INSERT INTO __EFMigrationsHistory (MigrationId, ProductVersion)
+VALUES ('20250226014320_MigrateFromWatsonORM', '9.0.2'); 
