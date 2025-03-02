8d8424d0f3b570f6e4a4dc4fbde7b77aee5e69b4
ItsRichardScull <totallynotrichardscull@gmail.com>
2025-03-02 09:29:17 +0200

    feat: Clear Hangfire jobs for SunriseServerFactory;

--- Sunrise.Tests/SunriseServerFactory.cs
+++ Sunrise.Tests/SunriseServerFactory.cs
@@ -1,15 +1,18 @@
 using System.Data.Common;
+using Hangfire;
+using Hangfire.Storage;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Sunrise.Shared.Application;
 using Sunrise.Shared.Database;
 
 namespace Sunrise.Tests;
 
-public class SunriseServerFactory : WebApplicationFactory<Server.Program>
+public class SunriseServerFactory : WebApplicationFactory<Server.Program>, IDisposable
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
@@ -35,5 +38,27 @@ public class SunriseServerFactory : WebApplicationFactory<Server.Program>
                 options.UseSqlite(connection).UseSeeding((ctx, _) => { DatabaseSeeder.UseAsyncSeeding(ctx).Wait(); });
             });
         });
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        var manager = new RecurringJobManager();
+        foreach (var job in JobStorage.Current.GetConnection().GetRecurringJobs())
+        {
+            manager.RemoveIfExists(job.Id);
+        }
+
+        using var connection = JobStorage.Current.GetConnection();
+
+        foreach (var job in connection.GetAllItemsFromSet("processing-jobs"))
+        {
+            BackgroundJob.Delete(job);
+        }
+        
+        foreach (var job in connection.GetAllItemsFromSet("scheduled-jobs"))
+        {
+            BackgroundJob.Delete(job);
+        }
+
     }
 }
