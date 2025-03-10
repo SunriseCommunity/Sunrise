17e757451c227031dc624e9b5db604f370e5f26f
ItsRichardScull <totallynotrichardscull@gmail.com>
2025-03-10 21:39:39 +0200

    fix: Parse CompactUser Id as nullable

--- Sunrise.Shared/Objects/Serializable/BeatmapSet.cs
+++ Sunrise.Shared/Objects/Serializable/BeatmapSet.cs
@@ -192,7 +192,7 @@ public class BeatmapSetDescription
 public class CompactUser
 {
     [JsonPropertyName("id")]
-    public int Id { get; set; }
+    public int? Id { get; set; }
 
     [JsonPropertyName("avatar_url")]
     public string AvatarUrl { get; set; }
