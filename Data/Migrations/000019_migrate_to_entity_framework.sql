34d4b11e08457aae1e82b15bf3c40ac71de04109
richardscull <totallynotrichardscull@gmail.com>
2025-03-28 20:12:11 +0200

    chore: Remove deprecated SQLite migrations;

--- Data/Migrations/000019_migrate_to_entity_framework.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
-      "MigrationId"	TEXT NOT NULL,
-      "ProductVersion"	TEXT NOT NULL,
-      CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY("MigrationId")
-);
-
-INSERT INTO __EFMigrationsHistory (MigrationId, ProductVersion)
-VALUES ('20250226014320_MigrateFromWatsonORM', '9.0.2'); 
--- Data/Migrations/000021_revert_unlifted_restrictions.sql
+++
@@ -1,9 +0,0 @@
-﻿-- Restricts all accidentally unrestricted users which were disabled
-
-UPDATE user
-SET AccountStatus = 1
-WHERE Id IN (
-    SELECT UserId FROM restriction 
-) AND AccountStatus = 2;
-
-SELECT 'Restrictions reverted'  AS TableName, changes() AS RevertedRestrictionsRows;
