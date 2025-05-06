8becad40733d1fb5a35a547b21e44b0145e76a34
richardscull <totallynotrichardscull@gmail.com>
2025-05-06 22:49:43 +0300

    fix: Remove mock of IsScoreable values;

--- Sunrise.Tests/Extensions/BeatmapExtensions.cs
+++ Sunrise.Tests/Extensions/BeatmapExtensions.cs
@@ -9,6 +9,5 @@ public static class BeatmapExtensions
     {
         beatmap.Checksum = score.BeatmapHash;
         beatmap.Id = score.BeatmapId;
-        beatmap.IsScoreable = score.IsScoreable;
     }
 }
