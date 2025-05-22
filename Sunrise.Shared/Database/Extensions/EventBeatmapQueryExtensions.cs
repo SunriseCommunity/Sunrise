eb6fe22a7a5c891e1a6cf6c8a7dce1a08ba42b66
richardscull <totallynotrichardscull@gmail.com>
2025-05-22 19:20:07 +0300

    fix: Include user meta files when including beatmap event executor;

--- Sunrise.Shared/Database/Extensions/EventBeatmapQueryExtensions.cs
+++ Sunrise.Shared/Database/Extensions/EventBeatmapQueryExtensions.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Sunrise.Shared.Database.Models;
 using Sunrise.Shared.Database.Models.Events;
 using Sunrise.Shared.Enums;
-using Sunrise.Shared.Enums.Beatmaps;
-using Sunrise.Shared.Enums.Scores;
-using Sunrise.Shared.Enums.Users;
-using Sunrise.Shared.Extensions.Beatmaps;
 
 namespace Sunrise.Shared.Database.Extensions;
 
@@ -13,6 +8,9 @@ public static class EventBeatmapQueryableExtensions
 {
     public static IQueryable<EventBeatmap> IncludeExecutor(this IQueryable<EventBeatmap> queryable)
     {
-        return queryable.Include(x => x.Executor);
+        return queryable
+            .Include(x => x.Executor)
+            .ThenInclude(u => u.UserFiles.Where(f => f.Type == FileType.Avatar || f.Type == FileType.Banner))
+            .AsSingleQuery();
     }
 }
