ad9fcca151e3718d3225676e24ef3d381ff6e6d4
richardscull <106016833+richardscull@users.noreply.github.com>
2025-07-04 08:39:19 +0300

    feat: Add migration to migrate data from score table to new split tables;

---
+++ Sunrise.Shared/Database/Migrations/20250704053500_MigrateDataFromScoreTableToNewSplitTables.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sunrise.Shared.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrateDataFromScoreTableToNewSplitTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            INSERT INTO score_metadata (Id, ScoreHash, OsuVersion, ClientTime)
+            SELECT Id, ScoreHash, OsuVersion, ClientTime FROM score;
+            ");
+            
+            migrationBuilder.Sql(@"
+            INSERT INTO score_hits (Id, MaxCombo, Count300, Count100, Count50, CountMiss, CountKatu, CountGeki) 
+            SELECT Id, MaxCombo, Count300, Count100, Count50, CountMiss, CountKatu, CountGeki FROM score;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
