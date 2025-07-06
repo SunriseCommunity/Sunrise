e96afde7284648af78ba726d31033bab2f6db137
richardscull <106016833+richardscull@users.noreply.github.com>
2025-07-06 04:51:53 +0300

    feat: Serialize object for websocket message with server's JsonOptions

--- Sunrise.API/Objects/WebSocketMessage.cs
+++ Sunrise.API/Objects/WebSocketMessage.cs
@@ -1,16 +1,20 @@
 ﻿using System.Text.Json;
 using Sunrise.API.Enums;
+using Sunrise.Shared.Application;
 
 namespace Sunrise.API.Objects;
 
 public class WebSocketMessage(WebSocketEventType type, object message)
 {
     private readonly object _message = message;
+    private JsonSerializerOptions JsonSerializerOptions { get; } = Configuration.SystemTextJsonOptions;
+
     public WebSocketEventType MessageType { get; } = type;
 
     public string Data => JsonSerializer.Serialize(new
-    {
-        type = MessageType,
-        data = _message
-    });
+        {
+            type = MessageType,
+            data = _message
+        },
+        JsonSerializerOptions);
 }
