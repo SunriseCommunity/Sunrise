acb6eebbf98b4847cc6709ee1f1ed1b148407e9f
richardscull <106016833+richardscull@users.noreply.github.com>
2025-07-15 19:53:35 +0300

    feat: Mock LocalProperties for BestScoreableRandomScore;

--- Sunrise.Tests/Services/Mock/Services/MockScoreService.cs
+++ Sunrise.Tests/Services/Mock/Services/MockScoreService.cs
@@ -107,6 +107,8 @@ public class MockScoreService(MockService service)
 
         score.Normalize();
 
+        score.LocalProperties = score.LocalProperties.FromScore(score);
+
         return score;
     }
 
