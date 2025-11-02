0418124cfef31d31aefa0f4c076166b4337d3485
richardscull <106016833+richardscull@users.noreply.github.com>
2025-11-02 09:36:42 +0200

    chore: Remove database logic (moved into fixture) from SunriseServerFactory

--- Sunrise.Tests/SunriseServerFactory.cs
+++ Sunrise.Tests/SunriseServerFactory.cs
@@ -1,4 +1,3 @@
-using System.Data;
 using Hangfire;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -16,10 +15,6 @@ namespace Sunrise.Tests;
 
 public class SunriseServerFactory : WebApplicationFactory<Server.Program>, IDisposable
 {
-    private static readonly SemaphoreSlim DatabaseLock = new(1, 1);
-    private static bool _schemaCreated;
-    private static List<string>? _cachedTableNames;
-
     public MockHttpClientService? MockHttpClient { get; private set; }
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
@@ -29,7 +24,7 @@ public class SunriseServerFactory : WebApplicationFactory<Server.Program>, IDisp
             var dbContextDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<SunriseDbContext>));
             if (dbContextDescriptor != null) services.Remove(dbContextDescriptor);
 
-            services.AddDbContextPool<SunriseDbContext>((container, options) =>
+            services.AddDbContextPool<SunriseDbContext>((_, options) =>
             {
                 options.EnableServiceProviderCaching(false);
                 options.EnableSensitiveDataLogging();
@@ -47,139 +42,16 @@ public class SunriseServerFactory : WebApplicationFactory<Server.Program>, IDisp
                 return MockHttpClient;
             });
 
-            var isShouldCreateSchema = !_schemaCreated;
+            using var scope = services.BuildServiceProvider().CreateScope();
 
-            DatabaseLock.Wait();
+            var database = scope.ServiceProvider.GetRequiredService<SunriseDbContext>();
 
-            try
-            {
-                if (!isShouldCreateSchema)
-                {
-                    using var tempScope = services.BuildServiceProvider().CreateScope();
-                    CleanupDatabaseInternalAsync(tempScope).GetAwaiter().GetResult();
+            if (!database.Database.GetDbConnection().Database.IsDatabaseForTesting())
+                throw new InvalidOperationException("Used database is not testing database. Are you trying to delete production data?");
 
-                    return;
-                }
-
-                using var scope = services.BuildServiceProvider().CreateScope();
-                var database = scope.ServiceProvider.GetRequiredService<SunriseDbContext>();
-
-                if (!database.Database.GetDbConnection().Database.IsDatabaseForTesting())
-                    throw new InvalidOperationException("Used database is not testing database. Are you trying to delete production data?");
-
-                database.Database.EnsureDeleted();
-                database.Database.EnsureCreated();
-
-                _schemaCreated = true;
-            }
-            finally
-            {
-                DatabaseLock.Release();
-            }
+            database.Database.EnsureDeleted();
+            database.Database.EnsureCreated();
         });
-    }
-
-    private static async Task CleanupDatabaseInternalAsync(IServiceScope scope)
-    {
-        var db = scope.ServiceProvider.GetRequiredService<SunriseDbContext>();
-
-        if (Configuration.UseCache)
-        {
-            try
-            {
-                var redis = scope.ServiceProvider.GetRequiredService<RedisRepository>();
-                await redis.Flush(flushOnlyGeneralDatabase: false);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Failed to flush Redis cache.", ex);
-            }
-        }
-
-        var tableNames = await GetTableNamesAsync(db);
-        var nonEmptyTables = new List<string>();
-
-        if (tableNames.Count == 0)
-            return;
-
-        var unionQuery = string.Join("\nUNION ALL\n",
-            tableNames.Select(table =>
-                $"SELECT '{table}' as table_name, EXISTS(SELECT 1 FROM `{table}` LIMIT 1) as has_data"));
-
-        var connection = db.Database.GetDbConnection();
-        if (connection.State != ConnectionState.Open)
-            await connection.OpenAsync();
-
-        try
-        {
-            await using var command = connection.CreateCommand();
-            command.CommandText = unionQuery;
-
-            await using var reader = await command.ExecuteReaderAsync();
-
-            while (await reader.ReadAsync())
-            {
-                var tableName = reader.GetString(0);
-                var hasData = reader.GetInt32(1);
-
-                if (hasData == 1)
-                {
-                    nonEmptyTables.Add(tableName);
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            throw new Exception($"Failed to check tables for data existence: {ex.Message}", ex);
-        }
-
-        if (nonEmptyTables.Count > 0)
-        {
-            var batchQuery = "SET FOREIGN_KEY_CHECKS = 0;\n";
-
-            foreach (var table in nonEmptyTables)
-            {
-                batchQuery += $"DELETE FROM `{table}`;\n";
-            }
-
-            batchQuery += "SET FOREIGN_KEY_CHECKS = 1;";
-
-#pragma warning disable EF1002
-            await db.Database.ExecuteSqlRawAsync(batchQuery);
-#pragma warning restore EF1002
-        }
-    }
-
-    private static async Task<List<string>> GetTableNamesAsync(SunriseDbContext db)
-    {
-        if (_cachedTableNames != null)
-            return _cachedTableNames;
-
-        var connection = db.Database.GetDbConnection();
-        if (connection.State != ConnectionState.Open)
-            await connection.OpenAsync();
-
-        var tableNames = new List<string>();
-
-        await using (var command = connection.CreateCommand())
-        {
-            command.CommandText = @"
-                SELECT TABLE_NAME 
-                FROM information_schema.TABLES 
-                WHERE TABLE_SCHEMA = DATABASE() 
-                AND TABLE_TYPE = 'BASE TABLE'
-                ORDER BY TABLE_NAME";
-
-            await using var reader = await command.ExecuteReaderAsync();
-
-            while (await reader.ReadAsync())
-            {
-                tableNames.Add(reader.GetString(0));
-            }
-        }
-
-        _cachedTableNames = tableNames;
-        return tableNames;
     }
 
     protected override void Dispose(bool disposing)
