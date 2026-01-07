f583fedeb3cee74ed9689f7fa9bee9191dc50bfd
richardscull <106016833+richardscull@users.noreply.github.com>
2026-01-07 21:49:03 +0200

    feat: add new tests

--- Sunrise.Server.Tests/Controllers/BanchoControllerTests.cs
+++ Sunrise.Server.Tests/Controllers/BanchoControllerTests.cs
@@ -1,11 +1,12 @@
 using HOPEless.Bancho;
 using HOPEless.Bancho.Objects;
-using Sunrise.Shared.Enums.Beatmaps;
+using osu.Shared;
 using Sunrise.Shared.Enums.Users;
 using Sunrise.Tests.Abstracts;
 using Sunrise.Tests.Services.Mock;
 using Sunrise.Tests.Utils;
-using Sunrise.Tests;
+using Configuration = Sunrise.Shared.Application.Configuration;
+using GameMode = Sunrise.Shared.Enums.Beatmaps.GameMode;
 
 namespace Sunrise.Server.Tests.Controllers;
 
@@ -31,7 +32,7 @@ public class BanchoControllerGetTests(IntegrationDatabaseFixture fixture) : Data
 public class BanchoControllerPostTests(IntegrationDatabaseFixture fixture) : BanchoTest(fixture)
 {
     private readonly MockService _mocker = new();
-    
+
     [Fact]
     public async Task TestOnInvalidPasshashRejectLogin()
     {
@@ -42,51 +43,153 @@ public class BanchoControllerPostTests(IntegrationDatabaseFixture fixture) : Ban
 
         var loginRequest = _mocker.User.GetUserLoginRequest(user);
         loginRequest.PassHash = "invalid_passhash";
-        
-        var authBody = GetUserBodyLoginRequest( loginRequest);
-        
+
+        var authBody = GetUserBodyLoginRequest(loginRequest);
+
         // Act
         var response = await client.PostAsync("/", new StringContent(authBody));
 
         // Assert
         response.EnsureSuccessStatusCode();
-        
+
         var responsePackets = await GetResponsePackets(response);
         var serverLoginReplyPacket = responsePackets.FirstOrDefault(x => x.Type == PacketType.ServerLoginReply);
         Assert.NotNull(serverLoginReplyPacket);
-        
+
         Assert.Equal((int)LoginResponse.InvalidCredentials, new BanchoInt(serverLoginReplyPacket.Data).Value);
     }
-    
+
     [Fact]
     public async Task TestReturnSuccessIfLoginRequestValid()
     {
         // Arrange
         var client = App.CreateClient().UseClient("c");
-        
+
         var user = await CreateTestUser();
         var authBody = GetUserBodyLoginRequest(_mocker.User.GetUserLoginRequest(user));
-        
+
         // Act
         var response = await client.PostAsync("/", new StringContent(authBody));
 
         // Assert
         response.EnsureSuccessStatusCode();
-        
+
         var responsePackets = await GetResponsePackets(response);
         var serverLoginReplyPacket = responsePackets.FirstOrDefault(x => x.Type == PacketType.ServerLoginReply);
         Assert.NotNull(serverLoginReplyPacket);
-        
+
         // We expect the server to return the user ID on successful login
         Assert.Equal(user.Id, new BanchoInt(serverLoginReplyPacket.Data).Value);
     }
-    
+
+
+    [Fact]
+    public async Task TestReturnExpectedLoginPacketsIfLoginRequestValid()
+    {
+        // Arrange
+        var client = App.CreateClient().UseClient("c");
+
+        var user = await CreateTestUser();
+        var authBody = GetUserBodyLoginRequest(_mocker.User.GetUserLoginRequest(user));
+
+        // Act
+        var response = await client.PostAsync("/", new StringContent(authBody));
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+
+        var responsePackets = await GetResponsePackets(response);
+
+        var expectedPacketsWithData = new List<(PacketType ServerLoginReply, object?)>
+        {
+            (PacketType.ServerLoginReply, new BanchoInt(user.Id)),
+            (PacketType.ServerBanchoVersion, new BanchoInt(19)),
+            (PacketType.ServerUserPermissions, new BanchoInt((int)(PlayerRank.Default | PlayerRank.Supporter))),
+            (PacketType.ServerChatChannelListingComplete, new BanchoInt(0)),
+            (PacketType.ServerChatChannelJoinSuccess, new BanchoString("#osu")),
+            (PacketType.ServerChatChannelJoinSuccess, new BanchoString("#announce")),
+            (PacketType.ServerChatChannelAvailable, new BanchoChatChannel
+            {
+                Name = "#osu",
+                Topic = "General chat channel.",
+                UserCount = 1
+            }),
+            (PacketType.ServerChatChannelAvailable, new BanchoChatChannel
+            {
+                Name = "#announce",
+                Topic = "Announcement chat channel.",
+                UserCount = 1
+            }),
+            // We could have more chat channels, but it's not critical to test them all here
+            (PacketType.ServerFriendsList, new BanchoIntList()),
+            (PacketType.ServerUserData, null),
+            (PacketType.ServerUserPresence, null),
+            (PacketType.ServerNotification, new BanchoString(Configuration.WelcomeMessage))
+        };
+
+
+        var expectedPacketsWithDataToNotExist = new List<(PacketType ServerLoginReply, object)>
+        {
+            (PacketType.ServerChatChannelJoinSuccess, new BanchoString("#staff"))
+        };
+
+
+        foreach (var (expectedPacketType, expectedData) in expectedPacketsWithData)
+        {
+            var packet = TakePacketWithSpecificData(responsePackets, expectedPacketType, expectedData);
+
+            Assert.NotNull(packet);
+        }
+
+        foreach (var (expectedPacketType, expectedData) in expectedPacketsWithDataToNotExist)
+        {
+            var packet = TakePacketWithSpecificData(responsePackets, expectedPacketType, expectedData);
+
+            Assert.Null(packet);
+        }
+    }
+
+    [Fact]
+    public async Task TestReturnFriendsListIncludeUserFriends()
+    {
+        // Arrange
+        var client = App.CreateClient().UseClient("c");
+
+        var user = await CreateTestUser();
+        var authBody = GetUserBodyLoginRequest(_mocker.User.GetUserLoginRequest(user));
+
+
+        var friendUser = _mocker.User.GetRandomUser();
+        await CreateTestUser(friendUser);
+
+        var relationship = await Database.Users.Relationship.GetUserRelationship(user.Id, friendUser.Id);
+        if (relationship == null)
+            throw new Exception("Failed to create user relationship for test.");
+
+        relationship.Relation = UserRelation.Friend;
+        await Database.Users.Relationship.UpdateUserRelationship(relationship);
+
+        // Act
+        var response = await client.PostAsync("/", new StringContent(authBody));
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+
+        var responsePackets = await GetResponsePackets(response);
+        var serverLoginReplyPacket = responsePackets.FirstOrDefault(x => x.Type == PacketType.ServerFriendsList);
+        Assert.NotNull(serverLoginReplyPacket);
+
+        var packetFriendsList = new BanchoIntList(serverLoginReplyPacket.Data).Value;
+
+        Assert.Contains(friendUser.Id, packetFriendsList);
+    }
+
     [Fact]
     public async Task TestReturnUserDataWithoutPerformanceOverflowIfLoginRequestValid()
     {
         // Arrange
         var client = App.CreateClient().UseClient("c");
-        
+
         var user = await CreateTestUser();
         var authBody = GetUserBodyLoginRequest(_mocker.User.GetUserLoginRequest(user));
 
@@ -94,40 +197,40 @@ public class BanchoControllerPostTests(IntegrationDatabaseFixture fixture) : Ban
         Assert.NotNull(userStats);
 
         userStats.PerformancePoints = _mocker.GetRandomDouble(false);
-        
+
         await Database.Users.Stats.UpdateUserStats(userStats, user);
-        
+
         // Act
         var response = await client.PostAsync("/", new StringContent(authBody));
 
         // Assert
         response.EnsureSuccessStatusCode();
-        
+
         var responsePackets = await GetResponsePackets(response);
         var serverUserDataPacket = responsePackets.FirstOrDefault(x =>
         {
             if (x.Type != PacketType.ServerUserData)
                 return false;
-            
+
             var packetData = new BanchoUserData(x.Data);
             return packetData.UserId == user.Id;
         });
-        
+
         Assert.NotNull(serverUserDataPacket);
-        
+
         var responseData = new BanchoUserData(serverUserDataPacket.Data);
         Assert.NotNull(responseData);
-        
+
         Assert.Equal(user.Id, responseData.UserId);
         Assert.Equal((short)userStats.PerformancePoints, responseData.Performance);
     }
-    
+
     [Fact]
     public async Task TestReturnUserDataWithPerformanceOverflowIfLoginRequestValid()
     {
         // Arrange
         var client = App.CreateClient().UseClient("c");
-        
+
         var user = await CreateTestUser();
         var authBody = GetUserBodyLoginRequest(_mocker.User.GetUserLoginRequest(user));
 
@@ -135,32 +238,71 @@ public class BanchoControllerPostTests(IntegrationDatabaseFixture fixture) : Ban
         Assert.NotNull(userStats);
 
         userStats.PerformancePoints = _mocker.GetRandomDouble(false) + ushort.MaxValue;
-        
+
         await Database.Users.Stats.UpdateUserStats(userStats, user);
-        
+
         // Act
         var response = await client.PostAsync("/", new StringContent(authBody));
 
         // Assert
         response.EnsureSuccessStatusCode();
-        
+
         var responsePackets = await GetResponsePackets(response);
         var serverUserDataPacket = responsePackets.FirstOrDefault(x =>
         {
             if (x.Type != PacketType.ServerUserData)
                 return false;
-            
+
             var packetData = new BanchoUserData(x.Data);
             return packetData.UserId == user.Id;
         });
-        
+
         Assert.NotNull(serverUserDataPacket);
-        
+
         var responseData = new BanchoUserData(serverUserDataPacket.Data);
         Assert.NotNull(responseData);
-        
+
         Assert.Equal(user.Id, responseData.UserId);
         Assert.Equal(0, responseData.Performance);
         Assert.Equal((long)userStats.PerformancePoints, responseData.RankedScore);
+    }
+
+
+    private BanchoPacket? TakePacketWithSpecificData(List<BanchoPacket> packets, PacketType expectedPacketType, object? expectedData)
+    {
+        var packetWithSameHeaderAndData = packets
+            .Where(x => x.Type == expectedPacketType)
+            .Select(x =>
+                {
+                    if (expectedData != null)
+                        return (x, Activator.CreateInstance(expectedData.GetType(), x.Data));
+
+                    return (x, null);
+                }
+            )
+            .FirstOrDefault(x =>
+            {
+                var (packet, packetData) = x;
+
+                if (expectedData == null)
+                {
+                    packets.Remove(packet);
+                    return true;
+                }
+
+                try
+                {
+                    Assert.Equivalent(packetData, expectedData);
+                    packets.Remove(packet);
+                    return true;
+                }
+                catch
+                {
+                }
+
+                return false;
+            });
+
+        return packetWithSameHeaderAndData.x;
     }
 }
