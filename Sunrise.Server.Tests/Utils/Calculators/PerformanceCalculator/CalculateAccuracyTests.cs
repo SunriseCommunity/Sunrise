534bb833b55d116ced72da266ba2f32660088098
richardscull <106016833+richardscull@users.noreply.github.com>
2026-05-23 20:45:40 +0300

    feat: Add tests for CalculateAccuracyTests SubmittedScore

--- Sunrise.Server.Tests/Utils/Calculators/PerformanceCalculator/CalculateAccuracyTests.cs
+++ Sunrise.Server.Tests/Utils/Calculators/PerformanceCalculator/CalculateAccuracyTests.cs
@@ -1,5 +1,6 @@
 ﻿using Sunrise.Shared.Database.Models;
 using Sunrise.Shared.Enums.Beatmaps;
+using Sunrise.Shared.Objects;
 using Sunrise.Tests.Abstracts;
 using Mods = osu.Shared.Mods;
 using PerformanceCalculatorClass = Sunrise.Shared.Utils.Calculators.PerformanceCalculator;
@@ -134,6 +135,218 @@ public class CalculateAccuracyTests : BaseTest
             CountMiss = 29,
             GameMode = GameMode.Mania,
             Mods = Mods.ScoreV2
+        };
+
+        // Act
+        var scoreAccuracy = PerformanceCalculatorClass.CalculateAccuracy(score);
+
+        // Assert
+        Assert.Equal(87.16, Math.Round(scoreAccuracy, 2));
+    }
+    
+    [Fact]
+    public void CalculateSubmittedScoreAccuracyForOsuStandardTest()
+    {
+        // Arrange
+        var score = new SubmittedScore
+        {
+            Count300 = 317,
+            CountGeki = 69,
+            Count100 = 38,
+            CountKatu = 22,
+            Count50 = 2,
+            CountMiss = 1,
+            GameMode = GameMode.Standard,
+            PlayerUsername = null,
+            ScoreHash = null,
+            BeatmapHash = null,
+            TotalScore = 0,
+            MaxCombo = 0,
+            Perfect = false,
+            Mods = Mods.None,
+            Grade = null,
+            IsPassed = false,
+            WhenPlayed = default,
+            OsuVersion = null,
+            ClientTime = default,
+            Accuracy = 0
+        };
+
+        // Act
+        var scoreAccuracy = PerformanceCalculatorClass.CalculateAccuracy(score);
+
+        // Assert
+        Assert.Equal(92.18, Math.Round(scoreAccuracy, 2));
+    }
+
+    [Fact]
+    public void CalculateSubmittedScoreAccuracyForEmptyScoreTest()
+    {
+        // Arrange
+        var score = new SubmittedScore
+        {
+            Count300 = 0,
+            CountGeki = 0,
+            Count100 = 0,
+            CountKatu = 0,
+            Count50 = 0,
+            CountMiss = 0,
+            GameMode = GameMode.Standard,
+            PlayerUsername = null,
+            ScoreHash = null,
+            BeatmapHash = null,
+            TotalScore = 0,
+            MaxCombo = 0,
+            Perfect = false,
+            Mods = Mods.None,
+            Grade = null,
+            IsPassed = false,
+            WhenPlayed = default,
+            OsuVersion = null,
+            ClientTime = default,
+            Accuracy = 0
+        };
+
+        // Act
+        var scoreAccuracy = PerformanceCalculatorClass.CalculateAccuracy(score);
+
+        // Assert
+        Assert.Equal(0, Math.Round(scoreAccuracy, 2));
+    }
+
+
+
+    [Fact]
+    public void CalculateSubmittedScoreAccuracyForOsuTaikoTest()
+    {
+        // Arrange
+        var score = new SubmittedScore
+        {
+            Count300 = 97,
+            CountGeki = 0,
+            Count100 = 16,
+            CountKatu = 0,
+            Count50 = 0,
+            CountMiss = 2,
+            GameMode = GameMode.Taiko,
+            PlayerUsername = null,
+            ScoreHash = null,
+            BeatmapHash = null,
+            TotalScore = 0,
+            MaxCombo = 0,
+            Perfect = false,
+            Mods = Mods.None,
+            Grade = null,
+            IsPassed = false,
+            WhenPlayed = default,
+            OsuVersion = null,
+            ClientTime = default,
+            Accuracy = 0
+        };
+
+        // Act
+        var scoreAccuracy = PerformanceCalculatorClass.CalculateAccuracy(score);
+
+        // Assert
+        Assert.Equal(91.30, Math.Round(scoreAccuracy, 2));
+    }
+
+    [Fact]
+    public void CalculateSubmittedScoreAccuracyForOsuCatchTheBeatTest()
+    {
+        // Arrange
+        var score = new SubmittedScore
+        {
+            Count300 = 84,
+            CountGeki = 11,
+            Count100 = 0,
+            CountKatu = 1,
+            Count50 = 155,
+            CountMiss = 0,
+            GameMode = GameMode.CatchTheBeat,
+            PlayerUsername = null,
+            ScoreHash = null,
+            BeatmapHash = null,
+            TotalScore = 0,
+            MaxCombo = 0,
+            Perfect = false,
+            Mods = Mods.None,
+            Grade = null,
+            IsPassed = false,
+            WhenPlayed = default,
+            OsuVersion = null,
+            ClientTime = default,
+            Accuracy = 0
+        };
+
+        // Act
+        var scoreAccuracy = PerformanceCalculatorClass.CalculateAccuracy(score);
+
+        // Assert
+        Assert.Equal(99.58, Math.Round(scoreAccuracy, 2));
+    }
+
+    [Fact]
+    public void CalculateSubmittedScoreAccuracyForOsuManiaTest()
+    {
+        // Arrange
+        var score = new SubmittedScore
+        {
+            Count300 = 2335,
+            CountGeki = 8321,
+            Count100 = 19,
+            CountKatu = 115,
+            Count50 = 11,
+            CountMiss = 27,
+            GameMode = GameMode.Mania,
+            PlayerUsername = null,
+            ScoreHash = null,
+            BeatmapHash = null,
+            TotalScore = 0,
+            MaxCombo = 0,
+            Perfect = false,
+            Mods = Mods.None,
+            Grade = null,
+            IsPassed = false,
+            WhenPlayed = default,
+            OsuVersion = null,
+            ClientTime = default,
+            Accuracy = 0
+        };
+
+        // Act
+        var scoreAccuracy = PerformanceCalculatorClass.CalculateAccuracy(score);
+
+        // Assert
+        Assert.Equal(99.19, Math.Round(scoreAccuracy, 2));
+    }
+
+    [Fact]
+    public void CalculateSubmittedScoreAccuracyForOsuManiaV2Test()
+    {
+        // Arrange
+        var score = new SubmittedScore
+        {
+            Count300 = 407,
+            CountGeki = 469,
+            Count100 = 61,
+            CountKatu = 203,
+            Count50 = 5,
+            CountMiss = 29,
+            GameMode = GameMode.Mania,
+            Mods = Mods.ScoreV2,
+            PlayerUsername = null,
+            ScoreHash = null,
+            BeatmapHash = null,
+            TotalScore = 0,
+            MaxCombo = 0,
+            Perfect = false,
+            Grade = null,
+            IsPassed = false,
+            WhenPlayed = default,
+            OsuVersion = null,
+            ClientTime = default,
+            Accuracy = 0
         };
 
         // Act
